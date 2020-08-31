eb840d1f256acc083a00b0095d3b9fb056572863
digant73 <diguardo@tiscali.it>
2020-08-31 16:57:02 +0200

    enabled classic jerk

--- artillery_swx1_marlin_fw_2.0.6.1/bltouch_waggster_mod/source/Marlin-2.0.x/Marlin/Configuration.h
+++ artillery_swx1_marlin_fw_2.0.6.1/bltouch_waggster_mod/source/Marlin-2.0.x/Marlin/Configuration.h
@@ -806,7 +806,7 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-//#define CLASSIC_JERK
+#define CLASSIC_JERK
 #if ENABLED(CLASSIC_JERK)
   #define DEFAULT_XJERK  8.0
   #define DEFAULT_YJERK  8.0
